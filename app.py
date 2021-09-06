0c68a04b31e1d75b44da44e00a2075ecff351891
pranav12060 <65703024+pranav12060@users.noreply.github.com>
2021-09-06 18:34:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,10 @@ model.fit(iris.data,iris.target)
 
 op = model.predict([[sl,sw,pl,pw]])
 op = iris.target_names[op[0]]
-st.title("The flower is Iris -",op)
+st.title(op)
+if(op=="setosa"):
+   st.image("https://images.unsplash.com/photo-1561831220-cc44b32786ca?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGlyaXMlMjBmbG93ZXJ8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")
+elif(op=="versicolor"):
+    st.image("https://images.unsplash.com/photo-1602008903362-c8647f986df5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80")
+else:
+    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkeb-oFhav9r_dcv8TmNFJ9JZVB-6iKJyYXA&usqp=CAU")
